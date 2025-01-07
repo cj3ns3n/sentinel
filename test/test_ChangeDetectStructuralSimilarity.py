68ce3b4043799cfcf5226f516b48bb60958d6791
Curtis Jensen <curtis.jensen@gmail.com>
2025-01-07 13:14:29 -0800

    change detect test

---
+++ test/test_ChangeDetectStructuralSimilarity.py
@@ -0,0 +1,20 @@
+import unittest
+import logging
+
+import cv2
+from ChangeDetectStructuralSimilarity import ChangeDetectStructuralSimilarity
+
+class TestStructuralSimilarity(unittest.TestCase):
+
+  def test_diff(self):
+    logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s - %(name)s', level=logging.INFO)
+    logger = logging.getLogger('test_dif')
+
+    image1 = cv2.imread('test/img1.jpg')
+    image2 = cv2.imread('test/img2.jpg')
+    changeDetect = ChangeDetectStructuralSimilarity(image1, image2, 400, 98, logger=logger)
+    self.assertEqual(1, len(changeDetect.diffContours))
+    self.assertEqual(317, len(changeDetect.diffContours[0]))
+
+if __name__ == '__main__':
+  unittest.main()
