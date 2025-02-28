6a136fb917aa642bc073d4272357f713d8814ce6
Curtis Jensen <curtis.jensen@gmail.com>
2025-02-27 21:06:15 -0800

    proper diff detection

--- ChangeDetectYolo.py
+++ ChangeDetectYolo.py
@@ -1,5 +1,7 @@
 from ultralytics import YOLO
 import numpy as np
+from time import time
+import cv2
 from logger import Logger
 
 
@@ -19,8 +21,11 @@ class ChangeDetectYolo:
   # end def
 
   def process(self, prevEvent, nextEvent):
+    startTime = time()
     self.logger.info('yolo "%s" change detection' % self.name)
     nextImg = nextEvent['img']
+    #nextImg = cv2.fastNlMeansDenoisingColored(nextImg, None, 10, 10, 7, 21)
+    self.logger.info('denoise time %f' % (time() - startTime))
 
     modelResp = self.model(nextImg)
     detections = modelResp[0]
@@ -35,7 +40,7 @@ class ChangeDetectYolo:
       # confidence is greater than the minimum confidence
       if confidence >= self.confidenceThreshold:
         id = int(data[5])
-        name = '%3d_%s' % (id, detections.names[id])
+        name = '%03d_%s' % (id, detections.names[id])
         self.logger.info('detected: %s: %f' % (name, confidence))
         diffAreas[name] = (data[:4])
         # if the confidence is greater than the minimum confidence,
@@ -43,10 +48,23 @@ class ChangeDetectYolo:
         #xmin, ymin, xmax, ymax = int(data[0]), int(data[1]), int(data[2]), int(data[3])
     # end for
 
-    if 'diffAreas' in prevEvent:
-      return {'diffAreas': self.findAOIs(prevEvent['diffAreas'], diffAreas), 'name': self.name}
+    self.logger.info('prev keys %s' % repr(prevEvent.keys()))
+    prevAois = {}
+    if 'detections' in prevEvent:
+      self.logger.info('detections: ' + str(len(detections)))
+      for detection in prevEvent['detections']:
+        self.logger.info('detection name: ' + detection['name'])
+        if detection['name'] == self.name:
+          prevAois = detection['diffAreas']
+
+    if len(prevAois) > 0:
+      self.logger.info('ADDING ** prev aois **')
+      return {'diffAreas': self.findAOIs(prevAois, diffAreas), 'name': self.name}
     else:
+      self.logger.info('ADDING ** no prev aois **')
       return {'diffAreas': diffAreas, 'name': self.name}
+
+    self.logger.info('detect time %f' % (time() - startTime))
   # end def
 
   def findAOIs(self, prevAreas, nextAreas):
@@ -54,18 +72,20 @@ class ChangeDetectYolo:
 
     for name, nextArea in nextAreas.items():
       if name not in self.ignores:
+        self.logger.info('curr name (%s) prev names (%s)' % (name, repr(prevAreas.keys())))
         if name in prevAreas:
           prevArea = prevAreas[name]
-          dist = np.linalg.norm(np.array((prevArea[0], prevArea[1])) - np.array(nextArea[0], nextArea[1]))
-          self.logger.info('dist 01 %f' % dist)
+          dist = np.linalg.norm(np.array((prevArea[0], prevArea[1])) - np.array((nextArea[0], nextArea[1])))
+          self.logger.info('dist 01 %f (%f)' % (dist, self.areaDiffThreshold))
           if dist > self.areaDiffThreshold:
             aois[name] = nextArea
           else:
-            dist = np.linalg.norm(np.array((prevArea[2], prevArea[3])) - np.array(nextArea[2], nextArea[3]))
-            self.logger.info('dist 02 %f' % dist)
+            dist = np.linalg.norm(np.array((prevArea[2], prevArea[3])) - np.array((nextArea[2], nextArea[3])))
+            self.logger.info('dist 02 %f (%f)' % (dist, self.areaDiffThreshold))
             if dist > self.areaDiffThreshold:
               aois[name] = nextArea
         else:
+          self.logger.info('ADDING curr name (%s) prev names (%s)' % (name, repr(prevAreas.keys())))
           aois[name] = nextArea
       # end if
     # end for
