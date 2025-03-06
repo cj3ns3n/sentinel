eff5791748cf70ba9716d822082f2ce8ca9ffd37
Curtis Jensen <curtis.jensen@gmail.com>
2025-03-06 12:06:46 -0800

    removed superfolous log methods

--- StorageObserver.py
+++ StorageObserver.py
@@ -24,7 +24,7 @@ class StorageObserver:
     def storeImage(self, imageContext):
         imgId = imageContext.acquireTimestamp
 
-        self.logInfo('storing image %s' % (imgId))
+        self.logger.info('storing image %s' % (imgId))
 
         fileName = imgId.replace(":", "-") + '.jpg'
         if self.zone:
@@ -34,26 +34,16 @@ class StorageObserver:
 
         if self.remoteUploader:
             try:
-                self.logInfo("uploading image: %s" % (fileName))
+                self.logger.info("uploading image: %s" % (fileName))
                 self.remoteUploader.upload(fileName)
-                self.logInfo("uploaded image: %s" % (fileName))
+                self.logger.info("uploaded image: %s" % (fileName))
             except Exception as err:
-                self.logErr("error uploading image: " + repr(err))
+                self.logger.error("error uploading image: " + repr(err))
         # end if
     # end def
 
     def saveImageFs(self, fileName, image):
-        self.logInfo('saved image: %s' % (fileName))
+        self.logger.info('saved image: %s' % (fileName))
         cv2.imwrite(fileName, image)
-    # end def
-
-    def logInfo(self, message):
-        threadName = threading.current_thread().name
-        self.logger.info(threadName + ' - ' + message)
-    # end def
-
-    def logErr(self, message):
-        threadName = threading.current_thread().name
-        self.logger.error(threadName + ' - ' + message)
     # end def
 # end class
